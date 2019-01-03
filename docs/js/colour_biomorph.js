48455f7a4a37a45413e1cf6bad73ea4d841b0363
Alan Canon <aronnax9000@gmail.com>
2019-01-02 19:35:42 -0500

    Add favicon, reduced margins and padding of tabs, removed tab borders

---
+++ docs/js/colour_biomorph.js
@@ -0,0 +1,435 @@
+// type
+//            SwellType = (Swell, Same, Shrink);
+//            chromosome = array[1..9] of INTEGER;
+//            CompletenessType = (Single, Double);
+//            SpokesType = (NorthOnly, NSouth, Radial);
+//            LimbType = (Stick, Oval, Rectangle);
+//            LimbFillType = (Open, Filled);
+//            person = record
+//                    gene: chromosome;
+//                    colorgene: array[1..8] of Longint;{index in clut}
+//                    BackColorGene: LongInt;{index in clut}
+//                    dgene: array[1..10] of SwellType;
+//                    SegNoGene: INTEGER;
+//                    SegDistGene: INTEGER;
+//                    CompletenessGene: CompletenessType;
+//                    SpokesGene: SpokesType;
+//                    tricklegene, mutsizegene, mutprobgene: INTEGER;
+//                    LimbShapeGene: LimbType;
+//                    LimbFillGene: LimbFillType;
+//                    ThicknessGene: INTEGER;
+//                    bioPicture: picHandle;
+//            end;
+var LimbType = {
+        Stick: 1,
+        Oval: 2,
+        Rectangle: 3,
+        properties: {
+            1: {name: "Stick"},
+            2: {name: "Oval"},
+            3: {name: "Rectangle"}
+        }
+}
+
+var LimbFillType = {
+        Open: 1,
+        Filled: 2,
+        properties: {
+            1: {name: "Open"},
+            2: {name: "Filled"},
+        }
+}
+
+/*
+ * Constructor for the Colour biomorph species.
+ * 
+ * A biomorph is associated with a watchmaker session, and a drawing object.
+ * 
+ * From the watchmaker session, the biomorph may draw global rules such as
+ * the settings for allowed mutations, and what sort of drawing context should
+ * be used to render images. The biomorph may also report changes
+ * in its state to the session. The supplied session object must include a property of
+ * 'species', a string containing the name of the registered species.
+ * 
+ * The drawing object is the document element representing the drawing surface
+ * for the biomorph's body. In the original implementation, this is an HTML canvas
+ * element, but support is contemplated for other drawing contexts, such as a SVG 
+ * Scalable Vector Graphic. 
+ * 
+ */
+function ColourBiomorph(session, drawer) {
+    console.log('new ColourBiomorph')
+    this.session = session
+    this.drawer = drawer
+    this.gene = chromosome()
+    this.dGene = new Array(10)
+    for(let i = 0; i < 10; i++) {
+        this.dGene[i] = SwellType.Same
+    }
+    this.segNoGene = 0
+    this.segDistGene = 0
+    this.completenessGene = CompletenessType.Sbingle
+    this.spokesGene = SpokesType.NorthOnly
+    this.trickleGene = TRICKLE
+    this.mutSizeGene = 0
+    this.mutProbGene = 0
+    this.colorGene = [0,0,0,0,0,0,0,0]
+    this.backColorGene = 0
+    this.limbShapeGene = LimbType.Stick;
+    this.limbFillGene = LimbFillType.Open;
+    this.thicknessGene = 1
+    this.pic = new ColourPic(this)
+    
+}
+
+ColourBiomorph.initializeSession = function(session) {
+    console.log('ColourBiomorph.initializeSession')
+}
+
+$.widget('dawk.colourbiomorph_geneboxes', {
+    options : {
+        engineering: true,
+        biomorph: null,
+    },
+    updateFromCanvas: function(canvas) {
+    },
+});
+
+// initializes the biomorph's genotype as one of a named set of types.
+ColourBiomorph.prototype.doPerson = function(morphType) {
+    console.log('ColourBiomorph doPerson ' + morphType)
+} 
+ColourBiomorph.prototype.doSaltation = function() {
+    console.log('ColourBiomorph.doSaltation')
+}
+// initializes the biomorph's genotype to a random set of values
+// causes the biomorph's genotype to undergo a random mutation
+ColourBiomorph.prototype.mutate = function() {
+    console.log('ColourBiomorph.mutate')
+}
+//creates and returns a new, mutated copy of the biomorph.
+ColourBiomorph.prototype.reproduce = function(element) {
+    return new ColourBiomorph(this.session, element)
+}
+// called when it is time for the biomorph to draw itself. 
+ColourBiomorph.prototype.develop = function() {
+    console.log('ColourBiomorph.develop')
+}
+ColourBiomorph.prototype.manipulate = function(geneboxIndex, leftRightPos, rung) {
+    console.log('ColourBiomorph.manipulate')
+}
+
+//Register the Colour biomorph species with the SpeciesFactory.
+_speciesFactorySingleton.registerSpeciesType("Colour", 
+        (function(session, drawer) { return new ColourBiomorph(session, drawer)}),
+        (function(session) { ColourBiomorph.initializeSession(session)}),
+        (function(geneboxes, geneboxes_options) { 
+            $.fn.colourbiomorph_geneboxes.call(geneboxes, geneboxes_options) }),
+        (function(geneboxes, canvas) { 
+            $(geneboxes).colourbiomorph_geneboxes('updateFromCanvas', canvas)}));
+
+
+
+
+
+
+
+/*
+ * Globals, line 247.
+ * 
+ * ColourLin = RECORD
+ *     StartPt, EndPt: Point;
+ *     Thickness: 1..8;
+ * END;
+ * LinPtr = ^ColourLin;
+ * LinHandle = ^LinPtr;
+ */
+
+function ColourLin(x, y, xnew, ynew, thick) {
+    this.startPt = new Point(x,y);
+    this.endPt = new Point(xnew,ynew);
+    this.thickness = thick;
+    this.nextLin = null;    
+//    this.id = 0;
+}
+
+ColourLin.prototype.linToString = function() {
+    return "ColourLin " + this.startPt.toString() + " -> " + this.endPt.toString() + " thickness " + this.thickness;
+}
+
+var PicStyleType = {LF: 1, RF: 2, FF: 3, LUD: 4, RUD:5, FUD:6, LSW:7, RSW:8, FSW:9,
+        properties: {
+            1: {name: "LF"},
+            2: {name: "RF"},
+            3: {name: "FF"},
+            4: {name: "LUD"},
+            5: {name: "RUD"},
+            6: {name: "FUD"},
+            7: {name: "LSW"},
+            8: {name: "RSW"},
+            9: {name: "FSW"}
+        }
+};
+var Compass = {NorthSouth:1, EastWest:2, properties: {
+    1: {name: "NorthSouth"}, 2:{name: "EastWest"}
+}};
+
+
+/*
+ * Globals, line 253.
+ *     ColourPic = RECORD
+ *          BasePtr: Ptr;
+ *          MovePtr: LinPtr;
+ *          Origin: Point;
+ *          PicSize: Integer;
+ *          PicPerson: person
+ *      END;
+ *      
+ * 
+ */
+function ColourPic(biomorph) {
+    this.biomorph = biomorph
+
+    this.basePtr = null // The first ColourLin
+    this.movePtr = null // The current ColourLin (used in walking the array)
+    this.origin = new Point(0,0) // a Point
+    this.picSize = 0 // Number of Lins
+    this.picPerson = null // the biomorph that this is a picture of.
+    this.margin = new Rect()
+}
+
+
+/*
+ PROCEDURE ZeroPic (VAR thisPic: ColourPic; Here: Point);
+    BEGIN
+        WITH thisPic DO
+            BEGIN
+                MovePtr = LinPtr(BasePtr);
+                PicSize = 0;
+                Origin = Here
+            END
+    END; {ZeroPic}
+ */
+ColourPic.prototype.zeroPic = function (here) {
+    if(this.basePtr != null) { 
+        // ColourPic has lines. Walk the singly linked list all the way to the end,
+        // disconnect each ColourLin from the next.
+        var walkPtr = this.basePtr;
+        while(walkPtr != null) {
+            // Gotta grab a reference to the next element in the list 
+            // before we disconnect it from the current one.
+            var nextLin = walkPtr.nextLin
+            walkPtr.nextLin = null;
+            walkPtr = nextLin;
+        }
+        this.picSize = 0;
+        this.origin = here;
+
+    }
+    this.margin = new Rect()
+    this.basePtr = null
+    this.movePtr = null
+    // console.log('zeroPic') 
+    // console.log(this)
+}
+
+
+/*
+ * PROCEDURE PicLine (VAR thisPic: ColourPic; x, y, xnew, ynew, thick: Integer);
+    BEGIN
+        IF thick > 8 THEN
+            thick = 8;
+        WITH thisPic DO
+            BEGIN
+                IF PicSize >= PicSizeMax THEN
+                    BEGIN
+{Message(GetString(TooLargeString));}
+ {used the help dialog! v1.1 changed to alert}
+                        DisplayError(-147, 'Biomorph too large, or other problem', ' ', StopError);
+                        ExitToShell
+                    END
+                ELSE
+                    WITH MovePtr^ DO
+                        BEGIN
+                            StartPt.h = x;
+                            StartPt.v = y;
+                            EndPt.h = xnew;
+                            EndPt.v = ynew;
+                            Thickness = Thick
+                        END;
+                MovePtr = linptr(size(MovePtr) + 10);  {advance 'array subscript' by number}
+{                                    of bytes occupied by one lin}
+                PicSize = PicSize + 1
+            END
+    END; {PicLine}
+
+ */
+ColourPic.prototype.picLine = function(x, y, xnew, ynew, thick) {
+//  // // console.log("picLine (" + x + "," + y + ")>(" + xnew + "," + ynew + ")" + " thickness " + thick);
+    if(thick > 8)
+        thick = 8;
+    if(this.picSize >= PICSIZEMAX) {
+        // {Message(GetString(TooLargeString));}
+        // {used the help dialog! v1.1 changed to alert}
+        alert('Biomorph too large, or other problem');
+        return
+    } else {
+        newLin = new ColourLin(x, y, xnew, ynew, thick)
+//        newLin.id = this.picSize++
+//        // console.log('added line ' + newLin.id)
+        if(this.basePtr == null) { // First ColourLin in the ColourPic.
+            this.basePtr = newLin; // set the base pointer to the new ColourLin
+        } else { // ColourPic already has at least one ColourLin.
+            // Link the new ColourLin onto the ColourLin at end of the ColourPic
+            this.movePtr.nextLin = newLin; 
+        }
+        this.movePtr = newLin; // Point to the new end of the list
+
+        var margin = this.margin;
+        if(x < margin.left)
+            margin.left = x;
+        else if(x > margin.right)
+            margin.right = x;
+        if(y > margin.bottom)
+            margin.bottom = y;
+        else if(y < margin.top)
+            margin.top = y;
+        if(xnew < margin.left)
+            margin.left = xnew;
+        else if(xnew > margin.right)
+            margin.right = xnew;
+        if(ynew > margin.bottom)
+            margin.bottom = ynew;
+        else if(ynew < margin.top)
+            margin.top = ynew;
+
+    }
+} // {PicLine}
+
+
+ColourPic.prototype.picToHtml = function() {
+    var html = PicStyleType.properties[this.picStyle].name;
+    return html;
+}
+
+
+ColourPic.prototype.actualLine = function(picStyle, orientation) {
+    var origin = this.origin;
+    var movePtr = this.movePtr;
+    var drawer = this.drawer;
+//    
+//  // console.log("actualLine Style:" + PicStyleType.properties[picStyle].name 
+//          + " id " + movePtr.id 
+//          + " movePtr:" + movePtr.toString() 
+//          + " Origin:" + origin.toString())
+
+    drawer.penSize(movePtr.thickness);
+    var x0;
+    var x1;
+    var y0;
+    var y1;
+    var startPt = movePtr.startPt;
+    var endPt = movePtr.endPt;
+    if(orientation == Compass.NorthSouth) {
+        y0 = startPt.v;
+        y1 = endPt.v;
+        x0 = startPt.h;
+        x1 = endPt.h;
+    } else {
+        y0 = startPt.h;
+        y1 = endPt.h;
+        x0 = startPt.v;
+        x1 = endPt.v;
+    }
+    switch(picStyle) {
+    case PicStyleType.LF: 
+        drawer.drawLine(x0, y0, x1, y1);
+        break;
+    case PicStyleType.RF: 
+        drawer.drawLine(-x0, y0, -x1, y1);
+        break;
+    case PicStyleType.FF: 
+        drawer.drawLine(x0, y0, x1, y1);
+        drawer.drawLine(-x0, y0, -x1, y1);
+        break;
+    case PicStyleType.LUD: 
+        drawer.drawLine(x0, y0, x1, y1);
+        drawer.drawLine(-x0, -y0, -x1, -y1);
+        break;
+    case PicStyleType.RUD: 
+        drawer.drawLine(-x0, y0, -x1, y1);
+        drawer.drawLine(x0, -y0, x1, -y1);
+        break;
+    case PicStyleType.FUD: 
+        drawer.drawLine(x0, y0, x1, y1);
+        drawer.drawLine(-x0, y0, -x1, y1);
+        drawer.drawLine(x0, -y0, x1, -y1);
+        drawer.drawLine(-x0, -y0, -x1, -y1);
+        break;
+    } // {CASES}
+} // {ActualLine}
+
+//{ColourPic already contains its own origin, meaning the coordinates at which}
+//{ it was originally drawn. Now draw it at place}
+
+ColourPic.prototype.drawPic = function(place) {
+    this.drawer = _drawerFactorySingleton.getDrawer('canvas2d', this.biomorph.drawer);
+
+    // console.log('drawPic picSize ' + this.picSize)
+    // console.log(this)
+    var drawer = this.drawer
+    var biomorph = this.biomorph
+    drawer.save()
+    drawer.translate(-place.h,-place.v);
+    if(false) { // draw bounding rectangle for debugging centring
+        drawer.setColor("red");
+        drawer.frameRect(this.margin);
+    }
+    var picStyle = PicStyleType.FF; 
+    switch(biomorph.completenessGene) {
+    case CompletenessType.Single: 
+        switch(biomorph.spokesGene) {
+        case SpokesType.NorthOnly: 
+            picStyle = PicStyleType.LF;
+            break;
+        case SpokesType.NSouth: 
+            picStyle = PicStyleType.LUD;
+            break;
+        case SpokesType.Radial: 
+            picStyle = PicStyleType.LUD;
+            break;
+        }
+        break;
+    case CompletenessType.Double: 
+        switch(biomorph.spokesGene) {
+        case SpokesType.NorthOnly: 
+            picStyle = PicStyleType.FF;
+            break;
+        case SpokesType.NSouth: 
+            picStyle = PicStyleType.FUD;
+            break;
+        case SpokesType.Radial: 
+            picStyle = PicStyleType.FUD;
+            break;
+        }
+        break;
+    }
+    drawer.penSize(biomorph.session.myPenSize);
+    // {reposition at base of grabbed space}
+    this.movePtr = this.basePtr;
+    drawer.setColor("black");
+
+    while(true) {
+        this.actualLine(picStyle, Compass.NorthSouth); // {sometimes rangecheck error}
+        if(biomorph.spokesGene == SpokesType.Radial) 
+            if(biomorph.completenessGene = CompletenessType.Single) 
+                this.actualLine(PicStyleType.RUD, Compass.EastWest);
+            else
+                this.actualLine(picStyle, Compass.EastWest);
+        if(this.movePtr.nextLin == null)
+            break; // Leave iteration with thisPic.movePtr pointing to the last ColourLin.
+        // Advance to next ColourLin.
+        this.movePtr = this.movePtr.nextLin;
+    }
+    drawer.penSize(1);
+} // {DrawPic}
